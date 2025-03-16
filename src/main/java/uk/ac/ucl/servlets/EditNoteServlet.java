38e1bffe5473102f62d238e014ffc5cf6318f798
raphaellith <raphaellith@gmail.com>
2025-03-16 19:01:02 +0000

    removed AbstractGetNoteServlet.java

--- src/main/java/uk/ac/ucl/servlets/EditNoteServlet.java
+++ src/main/java/uk/ac/ucl/servlets/EditNoteServlet.java
@@ -1,10 +1,34 @@
 package uk.ac.ucl.servlets;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import uk.ac.ucl.model.IndexEntryPath;
+import uk.ac.ucl.model.Model;
+import uk.ac.ucl.model.ModelFactory;
+import uk.ac.ucl.model.Note;
+
+import java.io.IOException;
 
 @WebServlet("/noteEditView.html")
-public class EditNoteServlet extends AbstractGetNoteServlet {
-    public EditNoteServlet() {
-        super("/noteEditView.jsp");
+public class EditNoteServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Model model = ModelFactory.getModel();
+        IndexEntryPath path = new IndexEntryPath(request.getParameter("path"));
+
+        Note note = (Note) model.getEntryByPath(path);
+
+        request.setAttribute("noteObj", note);
+        request.setAttribute("currentPath", path);
+
+        // Invoke the JSP
+        ServletContext context = getServletContext();
+        RequestDispatcher dispatch = context.getRequestDispatcher("/noteEditView.jsp");
+        dispatch.forward(request, response);
     }
 }
--- src/main/java/uk/ac/ucl/servlets/ViewNoteServlet.java
+++ src/main/java/uk/ac/ucl/servlets/ViewNoteServlet.java
@@ -1,10 +1,34 @@
 package uk.ac.ucl.servlets;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import uk.ac.ucl.model.IndexEntryPath;
+import uk.ac.ucl.model.Model;
+import uk.ac.ucl.model.ModelFactory;
+import uk.ac.ucl.model.Note;
+
+import java.io.IOException;
 
 @WebServlet("/noteView.html")
-public class ViewNoteServlet extends AbstractGetNoteServlet {
-    public ViewNoteServlet() {
-        super("/noteView.jsp");
+public class ViewNoteServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Model model = ModelFactory.getModel();
+        IndexEntryPath path = new IndexEntryPath(request.getParameter("path"));
+
+        Note note = (Note) model.getEntryByPath(path);
+
+        request.setAttribute("noteObj", note);
+        request.setAttribute("currentPath", path);
+
+        // Invoke the JSP
+        ServletContext context = getServletContext();
+        RequestDispatcher dispatch = context.getRequestDispatcher("/noteView.jsp");
+        dispatch.forward(request, response);
     }
 }
